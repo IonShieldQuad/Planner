ace168568a56f96a9a4cddf7af65c17c81da704a
IonShieldQuad <ion.shield.quad@gmail.com>
2021-05-28 13:34:34 +0300

    Removed old code and resources

--- app/src/main/java/com/ionshield/planner/activities/database/modes/Modes.java
+++
@@ -1,9 +0,0 @@
-package com.ionshield.planner.activities.database.modes;
-
-public enum Modes {
-    ITEMS(new ItemsMode()), TYPES(new TypesMode()), ITEM_ASSOC(new ItemAssocMode()), NODES(new NodesMode()), LOCATIONS(new LocationsMode()), EVENTS(new EventsMode()), PLANS(new PlansMode()), EVENT_ASSOC(new EventAssocMode()), LINK_TYPES(new LinkTypesMode()), LINKS(new LinksMode());
-    public Mode mode;
-    Modes(Mode mode) {
-        this.mode = mode;
-    }
-}
--- app/src/main/java/com/ionshield/planner/fragments/CustomMapViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.ionshield.planner.fragments;
-
-import androidx.lifecycle.ViewModel;
-
-public class CustomMapViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
