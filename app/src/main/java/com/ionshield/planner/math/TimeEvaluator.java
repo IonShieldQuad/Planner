becb7faee473d30b9342c7cf09dc66a2c0bc5090
IonShieldQuad <ion.shield.quad@gmail.com>
2021-06-10 22:51:57 +0300

    Added time constraint optimization

--- app/src/main/java/com/ionshield/planner/math/TimeEvaluator.java
+++ app/src/main/java/com/ionshield/planner/math/TimeEvaluator.java
@@ -7,6 +7,8 @@ import com.ionshield.planner.database.entities.Node;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.chrono.ChronoLocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +32,21 @@ public class TimeEvaluator {
                     currentTime = currentTime.plusSeconds(result.routes[0].legs[0].duration.inSeconds);
                 }
                 currentLocation = new LatLng(node.latitude, node.longitude);
+            }
+
+            //Wait if needed
+            if (eventData.event.isDateUsed) {
+                if (eventData.event.datetimeMin != null && currentTime.isBefore(ChronoLocalDateTime.from(eventData.event.datetimeMin))) {
+                    currentTime = LocalDateTime.from(eventData.event.datetimeMin);
+                }
+            }
+            else {
+                if (eventData.event.datetimeMin != null && currentTime.toLocalTime().isBefore(LocalTime.from(eventData.event.datetimeMin))) {
+                    currentTime = LocalDateTime.from(eventData.event.datetimeMin);
+                }
+                if (eventData.event.datetimeMax != null && currentTime.toLocalTime().isAfter(LocalTime.from(eventData.event.datetimeMax))) {
+                    currentTime = LocalDateTime.of(currentTime.toLocalDate().plusDays(1), eventData.event.datetimeMin != null ? eventData.event.datetimeMin.toLocalTime() : LocalTime.of(0, 0));
+                }
             }
 
             outData.add(new EventWithDateTimeData(eventData, currentTime, currentTime.plus(eventData.event.duration)));
